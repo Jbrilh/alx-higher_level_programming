105087ee686a2fad278c36f16bdae76f5aa58991
Jbril <Jbrilhdeveloper@gmail.com>
2023-11-01 14:19:18 +0000

    fizz buzz syntax fixed

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100):
+    for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("{}".format("FizzBuzz"), end=", ")
+            print("{}".format("FizzBuzz"), end=" ")
         if i % 3 == 0:
-            print("{}".format("Fizz"), end=", ")
+            print("{}".format("Fizz"), end=" ")
             continue
         if i % 5 == 0:
-            print("{}".format("Buzz"), end=", ")
+            print("{}".format("Buzz"), end=" ")
             continue
-        print("{:d}".format(i), end=", ")
-    print("{}".format("Buzz"), end=" ")
+        print("{:d}".format(i), end=" ")
