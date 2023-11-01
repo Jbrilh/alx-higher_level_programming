7333cce0faa1aa2b5501b3d45f29171520e17080
Jbril <Jbrilhdeveloper@gmail.com>
2023-11-01 14:12:17 +0000

    fizz buzz solved

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("{}".format("FizzBuzz"), end=", ")
+        if i % 3 == 0:
+            print("{}".format("fizz"), end=", ")
+            continue
+        if i % 5 == 0:
+            print("{}".format("buzz"), end=", ")
+            continue
+        print("{:d}".format(i), end=", ")
+
