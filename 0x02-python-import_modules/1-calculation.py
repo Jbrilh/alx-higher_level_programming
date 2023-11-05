a7b48f6a21e1a2c31bc2a45a588d32becae60e2c
Jbril <Jbrilhdeveloper@gmail.com>
2023-11-05 21:01:23 +0000

    different math function as a module

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,11 +3,7 @@ from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
     a = 10
     b = 5
-    summ = add(a, b)
-    subt = sub(a, b)
-    mult = mul(a, b)
-    divi = div(a, b)
-    print("{} + {} = {}".format(a, b, summ))
-    print("{} + {} = {}".format(a, b, subt))
-    print("{} + {} = {}".format(a, b, mult))
-    print("{} + {} = {}".format(a, b, divi))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, sub(a, b)))
+    print("{} + {} = {}".format(a, b, mul(a, b)))
+    print("{} + {} = {}".format(a, b, div(a, b)))
