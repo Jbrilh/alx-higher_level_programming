70475eb9dcadd8901af2cd4a2e1ad968853e9c42
Jbril <Jbrilhdeveloper@gmail.com>
2023-11-06 16:18:31 +0000

    list numbers in reversed list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse()
-    for lis in range(len(my_list)):
-        print("{:d}".format(my_list[lis]))
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
