569ece089e8e860dc30be0cf8a528a10167a9b08
Jbril <Jbrilhdeveloper@gmail.com>
2023-12-31 10:22:35 +0000

    first addition test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """0-add_integer
 The function "add_integer" returns the sum of two integers.
+Args:
+    a, b (int): integers values to be added
 """
 
 
 def add_integer(a, b=98):
-    """adds two integers function body"""
+    """adds two integers function body
+    Args:
+        a, b (int) integer values """
 
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
