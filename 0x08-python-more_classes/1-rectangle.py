48e3e7fa9d540f566da2e0866a80058b80d6502f
Jbril <Jbrilhdeveloper@gmail.com>
2024-01-03 16:55:07 +0000

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,25 +14,6 @@ class Rectangle:
         self.__height = height
 
     @property
-    def width(self):
-        """width getter"""
-
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """set the width value
-        Args:
-            value: value of the width, must be a positive int
-        """
-
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
     def height(self):
         """height getter"""
 
@@ -50,3 +31,21 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def width(self):
+        """width getter"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set the width value
+        Args:
+        value: value of the width, must be a positive int
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
