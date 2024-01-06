a24635044b09ad5669278222b14ab0b8d3fd8dd1
Jbril <Jbrilhdeveloper@gmail.com>
2024-01-06 13:33:56 +0000

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Return instance of specified class of the parent 
+    of that class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function to return instance of a class
+    Args:
+        obj (any): the object to be compared
+        a_class (type): the class that obj is compared to
+    Return:
+        Bolean of an instance of a class
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
