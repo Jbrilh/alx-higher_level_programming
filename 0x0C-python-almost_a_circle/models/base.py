59558cfcd3ea6a3337062b213616458551356558
Jbril <Jbrilhdeveloper@gmail.com>
2024-01-09 15:34:03 +0000

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,13 +5,16 @@
 class Base:
     """Base class"""
 
-__nb_objects = 0
+    __nb_objects = 0
+
     def __init__(self, id=None):
         """Base class constructor
         Args:
             id (int): integer value to id
         """
-        if id not None:
-            self.__id = id
+        if id is not None:
+            self.id = id
 
-
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
