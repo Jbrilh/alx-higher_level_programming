8e037e618ed00fcfa9300bddf1fd391983aac0db
Jbril <Jbrilhdeveloper@gmail.com>
2024-01-10 00:44:48 +0000

    validator

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,85 +1,87 @@
 #!/usr/bin/python3
-"""Rectangle class implement Base class"""
+"""Rectangle class implement Base class """
 
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle class implements Base class
+    """ rectangle class implement Base class
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
     def width(self):
         """
-            funciton to get width value
+            Returning private attribute (__width)
         """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-             function to set widht value
+            Setting private attribute (__width)
         """
-        self.setter_validator("width", value)
+        self.validator('width', value)
         self.__width = value
 
     @property
     def height(self):
         """
-            funciton to get height value
+            Returning private attribute (___height)
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-            function to set height value
+            Setting private attribute (__height)
         """
-        self.setter_validator("height", value)
+        self.validator('height', value)
         self.__height = value
 
     @property
     def x(self):
         """
-            funciton to get height x
+            Returning private attribute (__x)
         """
         return self.__x
 
     @x.setter
     def x(self, value):
         """
-            function to set x value
+            Setting private attribute (__x)
         """
-        self.setter_validator("x", value)
+        self.validator('x', value)
         self.__x = value
 
     @property
     def y(self):
-        """funciton to get y value"""
+        """
+            Returning private attribute (__y)
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """function to set y value
-            value (int): value to set to y
         """
-        self.setter_validator("y", value)
+            Setting private attribute (__y)
+        """
+        self.validator('y', value)
         self.__y = value
 
     @staticmethod
-    def setter_validator(attribute, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(attribute))
+    def validator(attribute, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(attribute))
         if attribute == "x" or attribute == "y":
             if value < 0:
-                raise ValueError("{} must be >= 0".format(attribute))
+                raise ValueError('{} must be >= 0'.format(attribute))
         elif value <= 0:
-                raise ValueError("{} must be > 0".format(attribute))
+            raise ValueError('{} must be > 0'.format(attribute))
