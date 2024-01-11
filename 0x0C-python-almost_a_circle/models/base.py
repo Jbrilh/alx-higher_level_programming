d25a495c4153abd49d0c603066b11ae82a54cb7a
Jbril <Jbrilhdeveloper@gmail.com>
2024-01-11 05:09:17 +0000

    with error

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """module Base"""
 
 import json
+import os
 
 
 class Base:
@@ -21,10 +22,30 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @classmethod
     def to_json_string(list_dictionaries):
         """method to change list to dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == "":
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return []
-
         return json.dumps(list_dictionaries)
+    
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save list object to JSON
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Return the desirialization of a JSON string.
+        """
+        if json_string is None or len(json_string) == 0:
+            return ("[]")
+        return json.loads(json_string)
