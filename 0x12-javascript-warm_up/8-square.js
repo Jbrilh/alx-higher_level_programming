c1dd5f8605c8a2953f9a14bb7f938ea660cf4e92
Jbril Ahmed <Jbrilhdeveloper@gmail.com>
2024-02-12 13:41:01 +0300

    square with proper standard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,10 +5,10 @@ if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   const size = Number(process.argv[2]);
 
   for (let i = 0; i < size; i++) {
-	  let row = '';
-	  for (let j = 0; j < size; j++) {
-		row += 'X';
-	  }
-	  console.log(row);
+    let row = '';
+    for (let j = 0; j < size; j++) {
+      row += 'X';
+    }
+    console.log(row);
   }
 }
