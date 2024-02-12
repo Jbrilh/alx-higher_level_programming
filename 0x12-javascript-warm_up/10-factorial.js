a90633d3af314992dafeaeb9fad98080030ea1a8
Jbril Ahmed <Jbrilhdeveloper@gmail.com>
2024-02-12 14:31:07 +0300

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,17 +2,13 @@
 
 function factorial (n) {
   if (isNaN(parseInt(n))) {
-	  return 1;
+    return 1;
+  } else if (parseInt(n) === 0 || parseInt(n) === 1) {
+    return 1;
   } else {
-      n = parseInt(n);
-      if (n === 0 || n === 1) {
-	      return 1;
-      } else {
-          return n * factorial(n - 1);
-      }
+    return n * factorial(n - 1);
   }
 }
-
-const input = process.argv[2];
-const result = factorial(input);
+const value = process.argv[2];
+const result = factorial(value);
 console.log(result);
